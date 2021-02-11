ef2bf2b26f4aa8188a07a95356b73e151550da30
Yukycookie <josephinezhang@hotmail.com>
2021-02-11 18:10:13 +0800

    third

--- src/project.js
+++ src/project.js
@@ -987,7 +987,7 @@ window.__require = function e(t, n, o) {
                         scale: .9
                     }).union().repeatForever().start()
                 }, t.prototype.update = function(e) {}, t.prototype.adsButtonFunc2 = function() {
-                    window.location.href = "https://weibo.com/u/5622416838" // 结束界面左边连接
+                    window.location.href = "https://www.douban.com/group/topic/211103413/?dt_dapp=1" // 结束界面左边连接
                 }, t.prototype.bannerButtonFunc = function() {
                     window.location.href = "" // 奖杯按钮
                 }, t.prototype.ShowFailedUi = function(e, t) {
